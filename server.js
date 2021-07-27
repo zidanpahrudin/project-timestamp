0c6b786e0359ff2c7668ff185af17e0abd12d3a0
Mustapa Zidan <zidan.pahrudin4@gmail.com>
2021-07-27 12:25:14 +0700

    serverjs rev

--- server.js
+++ server.js
@@ -54,22 +54,6 @@ app.get("/api/:date_string?", (req, res) => {
   }
 });
 
-// app.get("/api/:date_string?", (req, res) => {
-//   let dateString = req.params.date_string;
-//   if (/\d{5,}/.test(dateString)) {
-//     const dateInt = parseInt(dateString);
-
-//     res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
-//   } else {
-//     let dateObject = new Date(dateString);
-
-//     if (dateObject.toString() === "Invalid Date") {
-//       res.json({ error: "Invalid Date" });
-//     } else {
-//       res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
-//     }
-//   }
-// });
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
